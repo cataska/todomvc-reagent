2d8d00f3711a74306cd22a22227eb76305d6d4b5
Wen-Chun Lin <lovecankill@gmail.com>
2017-05-06 22:15:59 +0800

    Complete all

--- src/todomvc_reagent/core.cljs
+++ src/todomvc_reagent/core.cljs
@@ -32,11 +32,13 @@
   (swap! todos dissoc id))
 (defn save [title]
   (when (not-empty title) (add-todo title)))
-(defn clear-done []
+(defn clear-completed []
   (reset! todos (->> (vals @todos)
                      (filter :done)
                      (map :id)
                      (reduce dissoc @todos))))
+(defn complete-all [v]
+  (reset! todos (reduce #(assoc-in %1 [%2 :done] v) @todos (keys @todos))))
 
 ;; -------------------------
 ;; Views
@@ -57,14 +59,14 @@
                          27 (stop)
                          nil)}]])))
 
-(defn todo-item [todo]
-    (fn [{:keys [id title done]}]
-      [:li {:class (if done "completed" "")}
-       [:div.view
-        [:input.toggle {:type "checkbox" :checked done :on-change #(toggle id)}]
-        [:label title]
-        [:button.destroy {:on-click #(delete id)}]
-        [:input.edit {:value ""}]]]))
+(defn todo-item []
+  (fn [{:keys [id title done]}]
+    [:li {:class (if done "completed" "")}
+     [:div.view
+      [:input.toggle {:type "checkbox" :checked done :on-change #(toggle id)}]
+      [:label title]
+      [:button.destroy {:on-click #(delete id)}]
+      [:input.edit {:value ""}]]]))
 
 (defn todo-list [showing todos]
   [:ul.todo-list
@@ -75,36 +77,40 @@
                       todos)]
      ^{:key (:id todo)} [todo-item todo])])
 
-(defn todo-footer [showing count]
+(defn todo-footer [showing completed-cnt count]
   (let [a-fn (fn [kw url txt]
                [:a {:class (when (= kw @showing) "selected")
                     :href url
                     :on-click #(reset! showing kw)}
                 txt])]
-   (when (> count 0)
-     [:footer.footer
-      [:span.todo-count [:strong (str count)] " item left"]
-      [:ul.filters
-       [:li (a-fn :all "#/" "All")]
-       [:li (a-fn :active "#/active" "Active")]
-       [:li (a-fn :completed "#/completed" "Completed")]]
-      [:button.clear-completed {:on-click #(clear-done)} "Clear completed"]])))
+   [:footer.footer
+    [:span.todo-count [:strong (str count)] " item left"]
+    [:ul.filters
+     [:li (a-fn :all "#/" "All")]
+     [:li (a-fn :active "#/active" "Active")]
+     [:li (a-fn :completed "#/completed" "Completed")]]
+    (when (pos? completed-cnt)
+      [:button.clear-completed {:on-click #(clear-completed)} "Clear completed"])]))
 
 (defn home-page []
   (let [showing (r/atom :all)]
    (fn []
-     (let [items (vals @todos)]
+     (let [items (vals @todos)
+           completed-cnt (->> items (filter :done) count)
+           active-cnt (- (count items) completed-cnt)]
       [:div
        [:section.todoapp
         [todo-header "reagent" "What needs to be done?"]
 
-        [:section.main
-         [:input.toggle-all {:type "checkbox"}]
-         [:label {:for "toggle-all"} "Mark all as complte"]
+        (when (pos? (count items))
+          [:div
+           [:section.main
+            [:input.toggle-all {:type "checkbox" :checked (zero? active-cnt) :on-click #(complete-all (pos? active-cnt))}]
+            [:label {:for "toggle-all"} "Mark all as complte"]
 
-         [todo-list showing items]]
-      
-        [todo-footer showing (count items)]
+            [todo-list showing items]]
+
+           [todo-footer showing completed-cnt (count items)]])
         ]
      
        [:footer.info
